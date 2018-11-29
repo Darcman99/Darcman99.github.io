7961f8c6fc95ef390602bed89de349e3667974f4
Darcman99 <32188044+Darcman99@users.noreply.github.com>
2018-11-29 18:47:14 +1030

    Remove schedule module
    Look i've tried it, really want it to work, but at the moment i can't get it to work the way i want it to. I'll leave it for a later date.

--- scriptsheet.js
+++ scriptsheet.js
@@ -13,50 +13,12 @@ function date(){
 	var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
 	document.getElementById("date").innerHTML=months[date.getMonth()]+"&nbsp;"+numberToOrdinal(date.getDate())+",&nbsp;"+date.getFullYear();
 }
-function loadschedule(){
-	var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-	var fulldate=new Date();
-	var date=fulldate.getDate();
-	var nextday=new Date();
-	nextday.setDate(date+1);
-	var nextday2=new Date();
-	nextday2.setDate(date+2);
-	var nextday3=new Date();
-	nextday3.setDate(date+3);
-	var nextday4=new Date();
-	nextday4.setDate(date+4);
-	var nextday5=new Date();
-	nextday5.setDate(date+5);
-	var nextday6=new Date();
-	nextday6.setDate(date+6);
-	document.getElementById("currentdate").innerHTML=numberToOrdinal(date);
-	document.getElementById("currentday").innerHTML=days[fulldate.getDay()];
-	document.getElementById("date2").innerHTML=numberToOrdinal(nextday.getDate());
-	document.getElementById("date3").innerHTML=numberToOrdinal(nextday2.getDate());
-	document.getElementById("date4").innerHTML=numberToOrdinal(nextday3.getDate());
-	document.getElementById("date5").innerHTML=numberToOrdinal(nextday4.getDate());
-	document.getElementById("date6").innerHTML=numberToOrdinal(nextday5.getDate());
-	document.getElementById("date7").innerHTML=numberToOrdinal(nextday6.getDate());
-	document.getElementById("day2").innerHTML=days[nextday.getDay()];
-	document.getElementById("day3").innerHTML=days[nextday2.getDay()];
-	document.getElementById("day4").innerHTML=days[nextday3.getDay()];
-	document.getElementById("day5").innerHTML=days[nextday4.getDay()];
-	document.getElementById("day6").innerHTML=days[nextday5.getDay()];
-	document.getElementById("day7").innerHTML=days[nextday6.getDay()];
-}
 function refresh(){
 	location.reload();
 }
 function removefade(){
 	content.classList.remove("fade");
 	content.src="";
-}
-function schedule(){
-	clearTimeout(removefade,30000);
-	content.classList.add("fade");
-	content.src="schedule.html";
-	content.style.display="block";
-	setTimeout(removefade,30000);
 }
 function search(){
 	var input,filter,ul,li,a,i;input=document.getElementById("bookmarkssearch");
