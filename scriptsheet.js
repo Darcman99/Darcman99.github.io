5c6aeba8a798fd0b55ff56cdd8b1e1abde9eae53
Darcy Manoel <darcman00@gmail.com>
2019-06-23 14:57:22 +0930

    Alphabetise bookmarks after adding bookmark
    Again, works surprisingly well. Really enjoying making this bookmark feature.

--- scriptsheet.js
+++ scriptsheet.js
@@ -8,6 +8,24 @@ function addbookmark(){
 		document.getElementById("footer").innerHTML+='<a id="'+title+'" href="'+url+`">`+title+"</a>";
 	}
 	document.getElementById("addbookmark").blur();
+	var list,i,switching,b,shouldSwitch;
+	list=document.getElementById("footer");
+	switching=true;
+	while(switching){
+		switching=false;
+		b=list.getElementsByTagName("a");
+		for(i=0;i<(b.length-1);i++){
+			shouldSwitch=false;
+			if(b[i].innerHTML.toLowerCase()>b[i+1].innerHTML.toLowerCase()){
+				shouldSwitch=true;
+				break;
+			}
+		}
+		if(shouldSwitch){
+			b[i].parentNode.insertBefore(b[i+1],b[i]);
+			switching=true;
+		}
+	}
 }
 function date(){
 	var date=new Date();
