46718f530ab0c1302c2dfed5f3201f1115f94685
Darcy Manoel <darcman00@gmail.com>
2020-03-26 00:07:47 +1030

    Convert string definitions
    ` rather than " and '.

--- scriptsheet.js
+++ scriptsheet.js
@@ -1,18 +1,18 @@
 var armyHours=-1;
-var bat="";
-document.getElementById("bookmarks").innerHTML=localStorage.getItem("bookmarks");
+var bat=``;
+document.getElementById(`bookmarks`).innerHTML=localStorage.getItem(`bookmarks`);
 function addBookmark(){
-	var title=prompt("Title: ","Google");
-	var url=prompt("URL: ","https://www."+title.toLowerCase()+".com");
+	var title=prompt(`Title: `,`Google`);
+	var url=prompt(`URL: `,`https://www.`+title.toLowerCase()+`.com`);
 	if(title!=null||url!=null)
-		{document.getElementById("bookmarks").innerHTML+='<a id="'+title+'" href="'+url+`">`+title+"</a>";}
-	document.getElementById("addBookmark").blur();
+		{document.getElementById(`bookmarks`).innerHTML+=`<a id="`+title+`" href="`+url+`">`+title+`</a>`;}
+	document.getElementById(`addBookmark`).blur();
 	var list,i,switching,b,shouldSwitch;
-	list=document.getElementById("bookmarks");
+	list=document.getElementById(`bookmarks`);
 	switching=true;
 	while(switching){
 		switching=false;
-		b=list.getElementsByTagName("a");
+		b=list.getElementsByTagName(`a`);
 		for(i=0;i<(b.length-1);i++){
 			shouldSwitch=false;
 			if(b[i].innerHTML.toLowerCase()>b[i+1].innerHTML.toLowerCase()){
@@ -25,33 +25,33 @@ function addBookmark(){
 			switching=true;
 		}
 	}
-	localStorage.setItem("bookmarks",document.getElementById("bookmarks").innerHTML);
+	localStorage.setItem(`bookmarks`,document.getElementById(`bookmarks`).innerHTML);
 }
 function armyTime(){
 	armyHours=armyHours*-1;
 	time();
 }
 function getNumberWithOrdinal(n){
-	var s=["th","st","nd","rd"],
+	var s=[`th`,`st`,`nd`,`rd`],
 	v=n%100;
 	return n+(s[(v-20)%10]||s[v]||s[0]);
 }
 function keyPress(event){
 	if(event.keyCode==32){
-		document.getElementById("addBookmark").classList.toggle("hideButton");
-		document.getElementById("bookmarks").classList.toggle("hideBookmarks");
-		document.getElementById("percentage").classList.toggle("hidePercentage");
-		document.getElementById("removeBookmark").classList.toggle("hideButton");
+		document.getElementById(`addBookmark`).classList.toggle(`hideButton`);
+		document.getElementById(`bookmarks`).classList.toggle(`hideBookmarks`);
+		document.getElementById(`percentage`).classList.toggle(`hidePercentage`);
+		document.getElementById(`removeBookmark`).classList.toggle(`hideButton`);
 	}
 	else if(event.keyCode==9)
 		{location.reload();}
 }
 function removeBookmark(){
-	var title=prompt("Title: ","Google");
+	var title=prompt(`Title: `,`Google`);
 	if(title!=null)
 		{document.getElementById(title).remove();}
-	document.getElementById("removeBookmark").blur();
-	localStorage.setItem("bookmarks",document.getElementById("bookmarks").innerHTML);
+	document.getElementById(`removeBookmark`).blur();
+	localStorage.setItem(`bookmarks`,document.getElementById(`bookmarks`).innerHTML);
 }
 function time(){
 	var time=new Date();
@@ -59,40 +59,40 @@ function time(){
 	var hours=time.getHours();
 	var halfHours=hours;
 	var date=new Date();
-	var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
-	document.getElementById("date").innerHTML=months[date.getMonth()]+"&nbsp;"+getNumberWithOrdinal(date.getDate())+",&nbsp;"+date.getFullYear();
+	var months=[`January`,`February`,`March`,`April`,`May`,`June`,`July`,`August`,`September`,`October`,`November`,`December`];
+	document.getElementById(`date`).innerHTML=months[date.getMonth()]+`&nbsp;`+getNumberWithOrdinal(date.getDate())+`,&nbsp;`+date.getFullYear();
 	if(armyHours<0){
 		if(halfHours>12)
 			{halfHours=hours-12;}
 		else if(hours===0)
 			{halfHours=12;}
 		if(minutes<10)
-			{minutes="0"+minutes;}
+			{minutes=`0`+minutes;}
 		else
 			{minutes=minutes;}
 		if(hours<12)
-			{document.getElementById("ampm").innerHTML="AM";}
+			{document.getElementById(`ampm`).innerHTML=`AM`;}
 		else
-			{document.getElementById("ampm").innerHTML="PM";}
-		document.getElementById("time").innerHTML=halfHours+":"+minutes;
+			{document.getElementById(`ampm`).innerHTML=`PM`;}
+		document.getElementById(`time`).innerHTML=halfHours+`:`+minutes;
 	}
 	else{
 		if(minutes<10)
-			{minutes="0"+minutes;}
+			{minutes=`0`+minutes;}
 		else
 			{minutes=minutes;}
-		document.getElementById("ampm").innerHTML="";
-		document.getElementById("time").innerHTML=hours+":"+minutes;
+		document.getElementById(`ampm`).innerHTML=``;
+		document.getElementById(`time`).innerHTML=hours+`:`+minutes;
 	}
 }
 navigator.getBattery().then(function(battery){
-	battery.addEventListener("levelchange",function(){
-		document.getElementById("charge").style.width=(battery.level*100)+"vw";
-		document.getElementById("percentage").innerHTML=(Math.round(battery.level*100))+"%";
+	battery.addEventListener(`levelchange`,function(){
+		document.getElementById(`charge`).style.width=(battery.level*100)+`vw`;
+		document.getElementById(`percentage`).innerHTML=(Math.round(battery.level*100))+`%`;
 	}
 	)
-	document.getElementById("charge").style.width=(battery.level*100)+"vw";
-	document.getElementById("percentage").innerHTML=(Math.round(battery.level*100))+"%";
+	document.getElementById(`charge`).style.width=(battery.level*100)+`vw`;
+	document.getElementById(`percentage`).innerHTML=(Math.round(battery.level*100))+`%`;
 	bat=battery;
 }
 )
